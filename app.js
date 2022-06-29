c370bcbe7c684efb375bddb1cfb8cc89a8c468c6
elsonwong <elson.wong@live.com.sg>
2022-06-29 16:39:37 +0800

    added container and newImg

---
+++ app.js
@@ -0,0 +1,3 @@
+const container = document.querySelector('#container');
+const newImg = doucment.createElement('img');
+newImg.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
