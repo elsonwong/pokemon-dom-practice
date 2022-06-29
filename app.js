79ca927e3f60d53800cf6ae5b6cc30084a320966
elsonwong <elson.wong@live.com.sg>
2022-06-29 17:12:46 +0800

    added 151 pokemon

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const container = document.querySelector('#container');
-const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
-for(let i = 1; i < 10; i++) {
+for(let i = 1; i < 151; i++) {
     const newImg = document.createElement('img');
-    newImg.src = 
+    newImg.src = `${baseURL}${i}.png`
     container.appendChild(newImg)
 }
