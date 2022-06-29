0c9af02979de62a12b4a17e5aa6596c41d812a89
elsonwong <elson.wong@live.com.sg>
2022-06-29 17:16:58 +0800

    added div and span

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const container = document.querySelector('#container');
 const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
 for(let i = 1; i < 151; i++) {
+    const pokemon = document.createElement ('div');
+    const label = document.createElement ('span');
+    label.innerText = `#${i}`;
     const newImg = document.createElement('img');
     newImg.src = `${baseURL}${i}.png`
     container.appendChild(newImg)
